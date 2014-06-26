05010c85f6a4a6b38b7f670ed84ae847358a9ff3
limeng-mac <aiside@limeng-mac.local>
2014-06-26 16:51:05 +0800

    change no record action

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,24 +7,24 @@ class HomeController < ApplicationController
   end
 
   def update_artists
-    genre = Genre.find(params[:genre_id])
-    if (genre)
+  #  genre = Genre.find_by(:id, params[:genre_id])
+    genre = Genre.find_by(id: params[:genre_id])
+    if (!genre.nil?)
       @artists = genre.artists.map { |a| [a.name, a.id] }.insert(0, 'Select An Artist')
       @songs = genre.songs.map { |s| [s.title, s.id] }.insert(0, 'Select a Song')
     else
-      @artists = Artist.all
-      @songs = Song.all
+      @artists = Artist.all.map { |a| [a.name, a.id] }.insert(0, 'Select An Artist')
+      @songs = Song.all.map { |a| [a.title, a.id] }.insert(0, 'Select An Artist')
     end
 
   end
 
   def update_songs
-    artist = Artist.find(params[:artist_id])
-    if (artist)
+    artist = Artist.find_by(id: params[:artist_id])
+    if (!artist.nil?)
       @songs = artist.songs.map { |s| [s.title, s.id] }.insert(0, 'Select a Song')
     else
-      @songs = Song.all
-      @songs = Song.all
+      @songs = Song.all.map { |a| [a.title, a.id] }.insert(0, 'Select An Artist')
     end
   end
 
