f44f0a834519c2f2418bb7e7001582b419e064e0
limeng-mac <aiside@limeng-mac.local>
2014-06-27 16:25:08 +0800

    better display

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,30 +2,44 @@ class HomeController < ApplicationController
 
   def index
     @genres = Genre.all
-    @artists = Artist.all
-    @songs = Song.all
+
+    #@artists = Artist.all
+    #@songs = Song.all
+   # @artists = Array.new
+   # @songs = Array.new
+   # @artists = [{id: '', name: 'Select An Artist'}]
+   # @songs = [{id: '', title: 'Select An Song'}]
+    @artists = [Artist.new(id: '', name: 'Select an Artist')]
+    @songs = [Song.new(id: '', title: 'Select a Song')]
+    #@artists = @artists.map { |a| [a.name, a.id]  }.insert(0, 'Select An Artist')
+    #@songs = @songs.map { |s| [s.title, s.id] }.insert(0, 'Select A Songs')
   end
 
   def update_artists
   #  genre = Genre.find_by(:id, params[:genre_id])
     genre = Genre.find_by(id: params[:genre_id])
-    if (!genre.nil?)
-      @artists = genre.artists.map { |a| [a.name, a.id] }.insert(0, 'Select An Artist')
-      @songs = genre.songs.map { |s| [s.title, s.id] }.insert(0, 'Select a Song')
+    if (!genre.nil?) 
+   #  @artists = genre.artists.map { |a| [a.name, a.id] }.insert(0, 'Select An Artist')
+  #   @songs = genre.songs.map { |s| [s.title, s.id] }.insert(0, 'Select a Song')
+      @artists = genre.artists
+   #   @songs = genre.songs
     else
-      @artists = Artist.all.map { |a| [a.name, a.id] }.insert(0, 'Select An Artist')
-      @songs = Song.all.map { |a| [a.title, a.id] }.insert(0, 'Select An Artist')
+      @artists = Hash.new
+    #  
     end
-
+    @artists = @artists.map { |a| [a.name, a.id]  }.insert(0, 'Songselect An Artist')
+    @songs = Hash.new 
+    @songs = @songs.map { |s| [s.title, s.id] }.insert(0, 'Select A Songs')
   end
 
   def update_songs
     artist = Artist.find_by(id: params[:artist_id])
-    if (!artist.nil?)
-      @songs = artist.songs.map { |s| [s.title, s.id] }.insert(0, 'Select a Song')
+    if (!artist.nil?) 
+      @songs = artist.songs
     else
-      @songs = Song.all.map { |a| [a.title, a.id] }.insert(0, 'Select An Artist')
+      @songs = Hash.new
     end
+    @songs = @songs.map { |s| [s.title, s.id] }.insert(0, 'Select A Songs')
   end
 
 end
